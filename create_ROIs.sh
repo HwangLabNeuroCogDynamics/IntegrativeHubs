da27cfc7c43b6a811e553d95ceecf92f8c64b9da
Kai Hwang <kai.hwang@gmail.com>
2024-05-24 13:27:12 -0500

    commit.

---
+++ create_ROIs.sh
@@ -0,0 +1,45 @@
+## combine some ROIs...
+
+# /mnt/nfs/lss/lss_kahwang_hpc/ROIs
+
+# Schaeffer 400
+# Schaefer400_2.5.nii.gz
+
+# Morel
+# Morel_2.5_labels.nii.gz
+
+# BG
+# /Shared/lss_kahwang_hpc/ROIs/BG_Caudate_2.5mm.nii.gz
+# /Shared/lss_kahwang_hpc/ROIs/BG_Putamen_2.5mm.nii.gz
+# /Shared/lss_kahwang_hpc/ROIs/BG_Globus_Pallidus_2.5mm.nii.gz
+
+# all in 1.719 by 1.719 by 2.5 resolution
+# combine is Schaefer400+Morel+BG_2.5.nii.gz
+
+
+# label of ROIs
+
+# 0-400 Yeo cortical ROIs
+# see https://github.com/ThomasYeoLab/CBIG/tree/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI
+
+#Morel code
+#401 AN
+#402 VM
+#403 VL
+#404 MGN
+#405 MD
+#406 PuA
+#407 LP
+#408 IL
+#409 VA
+#410 Po
+#411 LGN
+#412 PuM
+#413 PuI
+#414 PuL
+#415 VP
+
+#BG code
+#416 Caudate
+#417 Putamen
+#418 GP
