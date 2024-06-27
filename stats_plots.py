3ec118fb53effa3d6f35d740dc6c95ad7c5781f8
Kai Hwang <kai.hwang@gmail.com>
2024-06-27 10:56:30 -0500

    commit.

--- stats_plots.py
+++ stats_plots.py
@@ -11,6 +11,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 from scipy.stats import ttest_1samp
+from nilearn import plotting
+from nilearn import datasets
+from nilearn import surface
+from statsmodels.stats.multitest import fdrcorrection
 plt.ion()
 
 def write_stats_to_vol_yeo_template_nifti(graph_metric, fn, roisize = 418):
@@ -29,8 +33,7 @@ def write_stats_to_vol_yeo_template_nifti(graph_metric, fn, roisize = 418):
 	nib.save(new_nii, fn) 
 	return new_nii
 
-### run group level stats and make niis or plots
-
+### load subject level output
 data_dir =  '/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/stats/'
 subjects = pd.read_csv("/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/3dDeconvolve_fdpt4/usable_subjs.csv")['sub']
 roi_fn = "whole_brain"
@@ -39,13 +42,7 @@ results = []
 for s in subjects:
     results.append(pd.read_csv(data_dir +"%s_%s_stats.csv" %(s, roi_fn)))
 results_df = pd.concat(results, ignore_index=True)
-
 models = results_df.parameter.unique()
-# models = ['EDS', 'IDS', 'Intercept', 'color', 'condition', 'context',
-#        'context:EDS', 'context:EDS:error', 'context:IDS', 'context:color',
-#        'context:shape', 'error', 'feature:EDS', 'feature:IDS',
-#        'feature:IDS:error', 'identity', 'response', 'shape', 'stim',
-#        'task', 'task:EDS', 'task:IDS']
 
 ### check null distribution
 nulls = np.zeros((59,418,len(models),4096)) #sub by roi by var by permutations
@@ -66,7 +63,7 @@ t_nulls = np.zeros((418,len(models),4096))
 for p in np.arange(4096):
      for r in np.arange(418):
           for v in np.arange(len(models)):
-               t_nulls[r,v,p], _= ttest_1samp(nulls[:,r,v,p], 0)
+               t_nulls[r,v,p], _= ttest_1samp(nulls[:,r,v,p], 0, nan_policy ='omit')
 
 
 df = pd.DataFrame(t_nulls[:,9,:].T) 
@@ -76,7 +73,86 @@ for col in np.arange(415,418):
 plt.show()
 ## remarkbly, the 97.5 percentile of the null seems to be around t=2. But with some diff between ROIs, so perhaps ROI specific null is a good idea.
 
-### run stats
+### run stats using empirical p
+permutation_stats = []
+for r in range(418): #results_df.ROI.unique()
+    for i, m in enumerate(models):
+        if i == 0: #no intercept
+            continue
+        else:
+            tdf = results_df.loc[(results_df['ROI']==r+1) & (results_df['parameter']==m)]
+            #model = smf.ols(formula="coef ~ 1", data=tdf).fit()
+            t_stat, _ = ttest_1samp(tdf['coef'], 0)
+            
+            ttdf = pd.DataFrame({
+                't-statistic': [t_stat],
+                'p-value': 1-(np.mean(t_stat>t_nulls[r,1:,:].flatten())),
+                'mean': [np.nanmean(tdf['coef'])],
+                'ROI': r+1,
+                'model': m
+            })
+            permutation_stats.append(ttdf)    
+permutation_stats_df = pd.concat(permutation_stats, ignore_index=True)
+permutation_stats_df['q'] = fdrcorrection(permutation_stats_df['p-value'])[1]
+permutation_stats_df.to_csv('/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/stats/permutation_stats_df.csv')
+
+
+# create thresholded nii images      
+thres_niis = {}
+for m in models:
+    if m != 'Intercept':
+        fn = '/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/niis/%s_t-statistic_thresholded.nii.gz' %m
+        metric = permutation_stats_df.loc[permutation_stats_df['model']== m]['t-statistic'].values
+        mask = permutation_stats_df.loc[permutation_stats_df['model']== m]['q'].values < .05
+        mask[np.argsort(metric)[:-10]]=0
+        thres_niis[m] = write_stats_to_vol_yeo_template_nifti(metric * mask, fn, roisize = 400)
+
+### create nii images
+# for m in models:
+#     fn = '/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/niis/%s_t-statistic.nii.gz' %m
+#     metric = permutation_stats_df.loc[permutation_stats_df['model']== m]['t-statistic'].values
+#     mask = 1
+#     write_stats_to_vol_yeo_template_nifti(metric * mask, fn, roisize = 418)
+
+plotting.plot_glass_brain(thres_niis['context'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+plotting.plot_glass_brain(thres_niis['shape'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+plotting.plot_glass_brain(thres_niis['color'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+plotting.plot_glass_brain(thres_niis['feature'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+plotting.plot_glass_brain(thres_niis['task'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+plotting.plot_glass_brain(thres_niis['response'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+plotting.plot_glass_brain(thres_niis['stim'], display_mode='lzry', plot_abs=False,
+                          title='', threshold=2)
+plt.show()
+
+permutation_stats_df.loc[permutation_stats_df['ROI']==405]
+
+
+# plot evoked responses
+EDS_v_IDS = nib.load("/home/kahwang/argon/data/ThalHi/3dMEMA/cluster_corrected_mema_EDS-IDS_59subs_tvalue.nii.gz")
+plotting.plot_glass_brain(EDS_v_IDS, display_mode='lzry', plot_abs=False,
+                          title='EDS - IDS', threshold=2)
+plt.show()
+
+
+### run parametric stats
 stats = []
 for r in results_df.ROI.unique():
     for m in models:
@@ -94,48 +170,21 @@ for r in results_df.ROI.unique():
 stats_df = pd.concat(stats, ignore_index=True)
 
 # FDR correction
-from statsmodels.stats.multitest import fdrcorrection
 stats_df['q'] = fdrcorrection(stats_df['p-value'])[1]
 stats_df.to_csv('/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/stats/group_stats.csv')
 
-### create nii images
-for m in models:
-    fn = '/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/niis/%s_t-statistic.nii.gz' %m
-    metric = stats_df.loc[stats_df['model']== m]['t-statistic'].values
-    mask = 1
-    write_stats_to_vol_yeo_template_nifti(metric * mask, fn, roisize = 418)
-
-# create thresholded nii images      
-thres_niis = {}
-for m in models:
-    fn = '/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/niis/%s_t-statistic_thresholded.nii.gz' %m
-    metric = stats_df.loc[stats_df['model']== m]['t-statistic'].values
-    mask = stats_df.loc[stats_df['model']== m]['q'].values < .05
-    thres_niis[m] = write_stats_to_vol_yeo_template_nifti(metric * mask, fn, roisize = 418)
-
-
 ### use nilearn plotting to visualize results
-
-from nilearn import datasets
-from nilearn import surface
 fsaverage = datasets.fetch_surf_fsaverage()
 curv_right = surface.load_surf_data(fsaverage.curv_right)
 curv_right_sign = np.sign(curv_right)
-texture = surface.vol_to_surf(thres_niis['context'], fsaverage.pial_right)
-
-from nilearn import plotting
+texture = surface.vol_to_surf(thres_niis['shape'], fsaverage.pial_right)
 
 fig = plotting.plot_surf_stat_map(
     fsaverage.infl_right, texture, hemi='right',
     title='Surface right hemisphere', colorbar=True,
-    threshold=1, bg_map=curv_right_sign,
+    threshold=0.1, bg_map=curv_right_sign,
 )
 fig.show()
-
-plotting.plot_glass_brain(thres_niis['context'], display_mode='lzry', plot_abs=False,
-                          title='Context', threshold=0.1)
-plt.show()
-
 
 ## plot PC
 pc_img = nib.load("/home/kahwang/bin/IntegrativeHubs/data/pc_vectors.nii.gz")
