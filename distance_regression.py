997e3373f7d28b1c24c7ab55610f0d22e64a8a7e
Kai Hwang <kai.hwang@gmail.com>
2025-04-04 14:48:30 -0500

    commit.

--- distance_regression.py
+++ distance_regression.py
@@ -13,6 +13,8 @@ import nilearn
 import scipy.linalg as linalg
 from joblib import Parallel, delayed
 from nilearn.plotting import plot_stat_map
+import os
+
 
 def write_stats_to_vol_yeo_template_nifti(graph_metric, fn, roisize = 418):
 	'''short hand to write vol based nifti file of the stats
@@ -105,7 +107,7 @@ def ds_regression_roi_per_subject(roi, data_dir, df, subjects, model_syntax):
                 })
         except Exception as e:
             # Insert NaNs for this subject and ROI in case of an error
-            for param in ["Stay:Accu:RT", "EDS:Accu:RT", "IDS:Accu:RT", "Cue", "Response"]:  # Update this list as needed
+            for param in model.params.index:  # Update this list as needed
                 results.append({
                     'ROI': roi,
                     'Subject': s,
@@ -205,17 +207,30 @@ subjects = pd.read_csv("/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/3dDeconvolve_fd
 ########################
 # Run main effect model
 ########################
-model_syntax = "ds ~ 0 + Stay:Cue_repeat + Stay:Cue_switch + IDS + EDS"
-results_list = Parallel(n_jobs=16)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
+model_syntax = "ds ~ 0 + Stay:Cue_repeat + Stay:Cue_switch + IDS + EDS + Response_repeat + Task_repeat "
+results_list = Parallel(n_jobs=10)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
 # Flatten the list of lists
 results_list = [item for sublist in results_list for item in sublist]
 # Create a DataFrame from the results
 results_df = pd.DataFrame(results_list)
 # Save the results to a CSV file
 results_df.to_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_main_effect.csv", index=False)
+results_df = pd.read_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_main_effect.csv")
 
 # group stats, key contrasts are EDS v IDS and IDS v Stay, eventually, look into cue repeat and resposnse repeat?
 param = 'EDS'
+group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
+write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
+
+param = 'IDS'
+group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
+write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
+
+param = 'Stay:Cue_repeat'
+group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
+write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
+
+param = 'Stay:Cue_switch'
 group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
 write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
 
@@ -229,10 +244,24 @@ param2 = 'Stay:Cue_switch'
 group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
 write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
 
+param1 = 'IDS'
+param2 = 'Stay:Cue_repeat'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+
 param1 = 'Stay:Cue_switch'
 param2 = 'Stay:Cue_repeat'
 group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
 write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+param = 'Response_repeat'
+group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
+write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
+
+param = 'Task_repeat'
+group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
+write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
 
 
 ########################
@@ -243,6 +272,7 @@ results_list = Parallel(n_jobs=16)(delayed(ds_regression_roi_per_subject)(roi, d
 results_list = [item for sublist in results_list for item in sublist]
 results_df = pd.DataFrame(results_list)
 results_df.to_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_cue_task_switch.csv", index=False)
+results_df = pd.read_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_cue_task_switch.csv")
 
 param1 = 'EDS:Task_switch'
 param2 = 'EDS:Task_repeat'
@@ -254,8 +284,64 @@ param2 = 'IDS:Task_switch'
 group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
 write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
 
+param1 = 'EDS:Task_switch'
+param2 = 'IDS:Task_switch'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
 param1 = 'EDS:Task_repeat'
 param2 = 'Stay:Cue_repeat'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+
+########################
+# Model 1.2, cue and task switch effect? as well as response repeat?
+########################
+model_syntax = (
+    "ds ~ 0 + Stay:Cue_repeat:Response_repeat + "
+    "Stay:Cue_switch:Response_repeat + "
+    "IDS:Task_switch:Response_repeat + "
+    "EDS:Task_repeat:Response_repeat + "
+    "EDS:Task_switch:Response_repeat + "
+    "Stay:Cue_repeat:Response_switch + "
+    "Stay:Cue_switch:Response_switch + "
+    "IDS:Task_switch:Response_switch + "
+    "EDS:Task_repeat:Response_switch + "
+    "EDS:Task_switch:Response_switch "
+)
+
+results_list = Parallel(n_jobs=20)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
+results_list = [item for sublist in results_list for item in sublist]
+results_df = pd.DataFrame(results_list)
+results_df.to_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_cue_task_response_switch.csv", index=False)
+results_df = pd.read_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_cue_task_response_switch.csv")
+
+# rule switch effect
+param1 = 'EDS:Task_repeat:Response_repeat'
+param2 = 'Stay:Cue_repeat:Response_repeat'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+param1 = 'EDS:Task_switch:Response_repeat'
+param2 = 'IDS:Task_switch:Response_repeat'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+# clean response effect
+param1 = 'Stay:Cue_repeat:Response_switch'
+param2 = 'Stay:Cue_repeat:Response_repeat'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+#clean task switch effect
+param1 = 'IDS:Task_switch:Response_repeat'
+param2 = 'Stay:Cue_switch:Response_repeat'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
+
+param1 = 'EDS:Task_switch:Response_repeat'
+param2 = 'EDS:Task_repeat:Response_repeat'
 group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1, param2)
 write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}_vs_{param2}")
 
@@ -263,8 +349,8 @@ write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home
 # Model 2, understand the effect of accuracy
 ########################
 
-model_syntax = "ds ~ 0 + Stay + IDS + EDS + Stay:Accu + IDS:Accu + EDS:Accu"
-results_list = Parallel(n_jobs=16)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
+model_syntax = "ds ~ 0 + Stay:Cue_repeat + Stay:Cue_switch + IDS + EDS + Stay:Accu + IDS:Accu + EDS:Accu + Stay:Cue_repeat:Accu + Stay:Cue_switch:Accu"
+results_list = Parallel(n_jobs=10)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
 results_list = [item for sublist in results_list for item in sublist]
 results_df = pd.DataFrame(results_list)
 results_df.to_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_accu_effect.csv", index=False)
@@ -278,21 +364,24 @@ param1 = 'IDS:Accu'
 group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1)
 write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}")
 
-param1 = 'Stay:Accu'
+param1 = 'Stay:Cue_switch:Accu'
 group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1)
 write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}")
 
-
+param1 = 'Stay:Cue_repeat:Accu'
+group_coefficients_diff, group_t_values_diff = group_level_stats_flexible(results_df, param1)
+write_group_stats_to_nifti(group_coefficients_diff, group_t_values_diff, f"/home/kahwang/bin/IntegrativeHubs/data/{param1}")
 
 ########################
 # Model 3, understand the effect of RT
 ########################
 
-model_syntax = "ds ~ 0 + Stay + IDS + EDS + Stay:RT + IDS:RT + EDS:RT"
-results_list = Parallel(n_jobs=16)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
+model_syntax = "ds ~ 0 + Stay:Cue_repeat + Stay:Cue_switch + IDS + EDS + Response_repeat + Task_repeat + Stay:Cue_repeat:RT + Stay:Cue_switch:RT + IDS:RT + EDS:RT"
+results_list = Parallel(n_jobs=8)(delayed(ds_regression_roi_per_subject)(roi, data_dir, df, subjects, model_syntax) for roi in np.arange(418))
 results_list = [item for sublist in results_list for item in sublist]
 results_df = pd.DataFrame(results_list)
 results_df.to_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_rt_effect.csv", index=False)
+results_df = pd.read_csv("/home/kahwang/bin/IntegrativeHubs/data/ds_regression_rt_effect.csv")
 
 param = 'EDS:RT'
 group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
@@ -302,17 +391,117 @@ param = 'IDS:RT'
 group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
 write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
 
-param = 'Stay:RT'
+param = 'Stay:Cue_switch:RT'
+group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
+write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
+
+param = 'Stay:Cue_repeat:RT'
 group_coefficients, group_t_values = group_level_stats_flexible(results_df, param)
 write_group_stats_to_nifti(group_coefficients, group_t_values, f"/home/kahwang/bin/IntegrativeHubs/data/{param}")
 
 
 
+########################
+# Just calculate ds and save it, maybe for parametric regression latter
+########################
 
-#### how to project data to surface? Looks nicer that way
+
+def save_ds_files_by_roi(data_dir, nii_dir, df, subjects, roi_list):
+    """
+    For each subject, compute the ds values (correlation distance) for each ROI,
+    adjust run-break trials to have ds = 0, compute condition multipliers from the df,
+    and then save the ds values into text files.
+    
+    The function creates the following files (inside a subject-specific directory):
+        - switch_distance.txt: The ds array for each trial (rows) and ROI (columns).
+        - EDS_switch_distance.txt: The ds array multiplied elementwise by EDS (binary vector).
+        - IDS_switch_distance.txt: The ds array multiplied elementwise by IDS.
+        - Stay_switch_distance.txt: The ds array multiplied elementwise by Stay.
+    
+    Parameters:
+        data_dir (str): Directory containing the subject npy files.
+        nii_dir (str): Base directory to save the text files.
+        df (pd.DataFrame): DataFrame containing subject information and condition labels.
+                           It must have a column 'sub' (subject ID) and a column 'Trial_type'.
+        subjects (list or pd.Series): List or Series of subject identifiers.
+        roi_list (list): List of ROI indices (1-indexed) for which ds values are computed.
+    
+    Returns:
+        None
+    """
+    # If subjects is a pandas Series, convert it to a list.
+    if isinstance(subjects, pd.Series):
+        subjects = subjects.tolist()
+
+    # Process each subject
+    for s in subjects:
+        # Build the file path and load the subject's whole-brain data.
+        # Data is expected to have shape: (n_rois, n_trials, n_trials)
+        file_path = os.path.join(data_dir, f"{s}_whole_brain_coef.npy")
+        data = np.load(file_path)
+        num_trials = data.shape[2]
+
+        # Define run-break trial indices (e.g., every 51st trial)
+        run_breaks = np.arange(0, num_trials, 51)
+
+        # Compute ds for each ROI and store in a list.
+        ds_roi_list = []
+        for roi in roi_list:
+            ds = np.zeros(num_trials)
+            # Compute ds using the "previous" trial (t-1 to t)
+            for t in range(1, num_trials):
+                # Note: roi is assumed to be 1-indexed; adjust to 0-indexed
+                ds[t] = data[roi - 1, t - 1, t]
+            # Convert correlation values to correlation distance
+            ds = np.sqrt(2 * (1 - ds))
+            # Instead of dropping run-break trials, set their ds values to 0.
+            ds[run_breaks] = 0
+            ds_roi_list.append(ds)
+
+        # Stack the ds vectors column-wise so that each row is a trial and each column is a ROI.
+        # Resulting shape: (n_trials, n_rois)
+        ds_array = np.column_stack(ds_roi_list)
+
+        # --- Retrieve condition information from df ---
+        # Expect df to have a 'Trial_type' column and a 'sub' column that matches s.
+        subject_df = df.loc[df['sub'] == s].reset_index(drop=True)
+        # Ensure that the number of trials in the DataFrame matches ds_array
+        if len(subject_df) != num_trials:
+            raise ValueError(f"Subject {s}: number of rows in df ({len(subject_df)}) "
+                             f"does not match number of trials ({num_trials}).")
+        
+        condition = subject_df['Trial_type'].values  # shape: (n_trials,)
+        # Create binary arrays for each condition.
+        EDS = (condition == "EDS").astype(int)   # 1 when condition is "EDS", else 0.
+        IDS = (condition == "IDS").astype(int)   # 1 when condition is "IDS", else 0.
+        Stay = (condition == "Stay").astype(int)   # 1 when condition is "Stay", else 0.
+        
+        # Multiply ds_array (2D: trials x ROIs) by the condition vectors (reshaped to be 2D)
+        ds_EDS  = ds_array * EDS[:, np.newaxis]
+        ds_IDS  = ds_array * IDS[:, np.newaxis]
+        ds_Stay = ds_array * Stay[:, np.newaxis]
+
+        # --- Save the arrays to text files ---
+        # Create a subject-specific directory if it doesn't exist.
+        sub_dir = os.path.join(nii_dir, f"sub-{s}")
+        os.makedirs(sub_dir, exist_ok=True)
+
+        # Save each array using np.savetxt.
+        np.savetxt(os.path.join(sub_dir, "switch_distance.txt"), ds_array, fmt="%.6f")
+        np.savetxt(os.path.join(sub_dir, "EDS_switch_distance.txt"), ds_EDS, fmt="%.6f")
+        np.savetxt(os.path.join(sub_dir, "IDS_switch_distance.txt"), ds_IDS, fmt="%.6f")
+        np.savetxt(os.path.join(sub_dir, "Stay_switch_distance.txt"), ds_Stay, fmt="%.6f")
+
+        print(f"Saved ds files for subject {s} in {sub_dir}")
+
+# Example usage:
+# Assuming you have the variables: data_dir, nii_dir, df, subjects, and roi_list defined,
+# you can call:
+#
+# save_ds_files_by_roi(data_dir, nii_dir, df, subjects, roi_list)
 
 
-
+ds_by_subject = save_ds_files_by_roi(data_dir, nii_dir, df, subjects, roi_list)
 
 ####################
 # graveyard
