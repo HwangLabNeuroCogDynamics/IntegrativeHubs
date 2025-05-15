a63e7bb2c3df09f46934267107f743f68e478af9
kaihwang <kai.hwang@gmail.com>
2025-05-15 10:01:12 -0500

    commit.

--- test_autocorr.py
+++ test_autocorr.py
@@ -1,55 +1,50 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 plt.ion()
 
-data = np.load('/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/coefs/10001_whole_brain_rtcov_coef.npy')
-M = data.mean(axis =0)
-n_runs   = 8
-run_size = M.shape[0] // n_runs   # 408/8 = 51
-# reshape into (run₁, trial₁, run₂, trial₂)
-M4 = M.reshape(n_runs, run_size,
-               n_runs, run_size)
-
-# average across run₁ and run₂ axes → (trial₁, trial₂) = (51, 51)
-M_avg = M4.mean(axis=(0, 2))
-
+subjects = pd.read_csv("/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/3dDeconvolve_fdpt4/usable_subjs.csv")['sub'] #this is usable subjects after Steph's filtering.
+M = np.zeros((len(subjects), 408, 408))
+for i, sub in enumerate(subjects):
+    data = np.load('/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/RSA/trialwiseRSA/coefs/%s_whole_brain_rtcov_coef.npy' %sub)
+    if data.shape[1] == 408:
+        M[i,:,:] = np.nanmean(data, axis=0)
+M_avg = np.nanmean(M, axis=0)
 print(M_avg.shape) 
+
+D = np.zeros((len(subjects), 408, 408))
+for i, sub in enumerate(subjects):
+    data = np.load('/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/GLMsingle/trialwiseRSA/rdms_correlation_whitened_betas/%s_Schaefer400_Morel_BG_rdm_corr_whitened.npy' %sub)
+    if data.shape[1] == 408:
+         D[i,:, :] = np.nanmean(data, axis=0)
+
+D_avg = np.nanmean(D, axis=0)
+
 
 plt.figure()
 sns.heatmap(
-    M_avg - M_avg.mean(),
-    vmin=-0.05,      # set lower end of color scale
-    vmax=0.05,       # set upper end
-    center=0,     # ensures zero is in the middle of the colormap
-    square=True,  # make cells square
-    cbar_kws={'label': 'Value'}  # label for the color bar
+    M_avg,
+    vmin=-0.4,      
+    vmax=0.4,       
+    center=0,     
+    square=True,  
+    cmap = "vlag",
 )
 plt.title('Trialwise Corr Coefficient Matrix for LSS', fontsize=20)
 plt.tight_layout()
 plt.show()
 
-data = np.load('/mnt/nfs/lss/lss_kahwang_hpc/data/ThalHi/GLMsingle/trialwiseRSA/coefs/10001_whole_brain_rtcov_coef.npy')
-M = data.mean(axis =0)
-n_runs   = 8
-run_size = M.shape[0] // n_runs   # 408/8 = 51
-# reshape into (run₁, trial₁, run₂, trial₂)
-M4 = M.reshape(n_runs, run_size,
-               n_runs, run_size)
-
-# average across run₁ and run₂ axes → (trial₁, trial₂) = (51, 51)
-M_avg = M4.mean(axis=(0, 2))
 
 print(M_avg.shape) 
 plt.figure()
 sns.heatmap(
-    M_avg - M_avg.mean(),
-    vmin=-0.05,      # set lower end of color scale
-    vmax=0.05,       # set upper end
+    D_avg ,
+    vmin=-0.4,      
+    vmax=0.4,       
     center=0,     # ensures zero is in the middle of the colormap
-    square=True,  # make cells square
-    cbar_kws={'label': 'Value'}  # label for the color bar
+    square=True,  
+    cmap = "vlag",
 )
 plt.title('Trialwise Corr Coefficient Matrix for GLMsingle', fontsize=20)
 plt.tight_layout()
